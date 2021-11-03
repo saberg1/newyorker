5226f707f8a403e22d0da13902930d07ce0b4f49
StevenTheBearded <stevenberg22@gmail.com>
2021-11-03 13:04:27 -0400

    Add JSX to navigation elemnt

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import './Navigation.css'
+
+const Navigation = () => {
+
+  return (
+    <nav>
+      {/* <div className='divider-top' />
+      <div className='divider-bottom' /> */}
+        <span className='nav-container'>
+          <h1>World</h1>
+          <h1>U.S.</h1>
+          <h1>Tech</h1>
+          <h1>Politics</h1>
+          <h1>Science</h1>
+          <h1>Business</h1>
+          <h1>Health</h1>
+          <h1>Sports</h1>
+          <h1>Food</h1>
+          <h1>Travel</h1>
+        </span>
+      <div className='divider top' />
+      <div className='divider bottom' />
+    </nav>
+  )
+}
+
+export default Navigation
